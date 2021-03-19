dcfa9395695fa1718bec0c3fe00458b12ca60a79
Magne Cedric <c.magne@magensys.com>
2021-03-19 16:49:01 +0100

    Design index page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
+import React, { useContext } from "react";
 import logo from "./logo.svg";
+import Web3Context from "context/Web3Context";
 
 function App() {
+  const { web3 } = useContext(Web3Context);
+
   return (
-    <section className="App-header">
-      <img src={logo} className="" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </section>
+    <div>
+      {web3 ? (
+        <>
+          <section className="text-center bg-white">
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl mb-8">
+              <span className="block xl:inline">Welcome to ReactJS </span>
+              <span className="block text-indigo-600 xl:inline">
+                with Truffle boxes
+              </span>
+            </h1>
+            <div className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl">
+              <p>
+                Your Truffle Box is installed and ready.
+                <br />
+              </p>
+              <img src={logo} alt="" />
+            </div>
+          </section>
+        </>
+      ) : (
+        <div>Loading Web3, accounts, and contract...</div>
+      )}
+    </div>
   );
 }
 
